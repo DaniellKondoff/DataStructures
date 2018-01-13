c88e29ccb22d3f650b8eec5f786124415447c3a0
Daniel Kondov <danielkondov91@gmail.com>
2018-01-13 13:50:18 +0200

    LAB

---
+++ 2.LINEAR DATA STRUCTURES - STACKS AND QUEUES/StackAndQueues/MatchingBrackets/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const char openBracket = '(';
+            const char closedtBracket = ')';
+
+            string input = "1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5";
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+
+                if (current == openBracket)
+                {
+                    stack.Push(i);
+                }
+                else if(current == closedtBracket)
+                {
+                    int startIndex = stack.Pop();
+                    int length = i - startIndex + 1;
+                    string contents = input.Substring(startIndex, length);
+                    Console.WriteLine(contents);
+                }
+            }
+
+        }
+    }
+}
---
+++ 2.LINEAR DATA STRUCTURES - STACKS AND QUEUES/StackAndQueues/NodeDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> head = new Node<int>(2);
+            Node<int> next = new Node<int>(10);
+
+            head.Next = next;
+
+        }
+    }
+}
