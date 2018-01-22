64d764170256516b214860d3c73b543153942fc5
Daniel Kondov <danielkondov91@gmail.com>
2018-01-23 00:19:00 +0200

    LAB

---
+++ 3. BASIC TREE DATA STRUCTURES - TREE, BINARY TREE/TreesDemo/TreesHW2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    static Dictionary<int, Tree<int>> nodes = new Dictionary<int, Tree<int>>();
+
+    static void Main(string[] args)
+    {
+        ReadTree();
+
+        Tree<int> root = GetRoot();
+
+        //5 Deepest Node
+        var deepestNodes = GetDeepestNode(root);
+
+        foreach (var node in deepestNodes)
+        {
+            Print(node);
+            Console.WriteLine();
+        }
+    }
+
+    private static void Print(Tree<int> node)
+    {
+        Console.Write(node.Value + " ");
+
+        foreach (var child in node.Children)
+        {
+            Print(child);
+        }
+    }
+
+    private static void ReadTree()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            var edge = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Tree<int> parent = GetNode(edge[0]);
+            Tree<int> child = GetNode(edge[1]);
+
+            parent.Children.Add(child);
+            child.Parent = parent;
+        }
+    }
+
+    private static Tree<int> GetNode(int value)
+    {
+        if (!nodes.ContainsKey(value))
+        {
+            nodes[value] = new Tree<int>(value);
+        }
+
+        return nodes[value];
+    }
+
+    private static Tree<int> GetRoot()
+    {
+        return nodes.Values.Where(x => x.Parent == null).FirstOrDefault();
+    }
+
+    private static IEnumerable<Tree<int>> GetDeepestNode(Tree<int> node)
+    {
+        var target = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Subtrees of sum {target}:");
+
+        var leafs = new List<Tree<int>>();
+
+        var sum = DFS(node, target, 0, leafs);
+
+        return leafs;
+    }
+
+    private static int DFS(Tree<int> node, int targetSum, int currentSum, List<Tree<int>> leafs)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        currentSum = node.Value;
+       
+
+        foreach (var child in node.Children)
+        {
+          currentSum +=  DFS(child, targetSum, currentSum, leafs);
+        }
+
+        if (currentSum == targetSum)
+        {
+            leafs.Add(node);
+        }
+
+        return currentSum;
+    }
+}
+
---
+++ 4.BINARY SEARCH TREES/BinarySearchTrees/Trees/BinarySearchTree.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+public class BinarySearchTree<T> where T : IComparable
+{
+    private class Node
+    {
+        public T Value { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    private Node root;
+
+    public BinarySearchTree()
+    {
+
+    }
+
+    private BinarySearchTree(Node node)
+    {
+        this.Copy(node);
+    }
+
+    private void Copy(Node node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.Insert(node.Value);
+        this.Copy(node.Left);
+        this.Copy(node.Right);
+    }
+
+    public void Insert(T element)
+    {
+        if (this.root == null)
+        {
+            this.root = new Node(element);
+            return;
+        }
+
+        Node parent = null;
+        Node current = this.root;
+
+        while (current != null)
+        {
+            parent = current;
+
+            if (current.Value.CompareTo(element) > 0)
+            {
+                current = current.Left;
+            }
+            else if (current.Value.CompareTo(element) < 0)
+            {
+                current = current.Right;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        current = new Node(element);
+        if (parent.Value.CompareTo(element) < 0)
+        {
+            parent.Right = current;
+        }
+        else
+        {
+            parent.Left = current;
+        }
+    }
+
+    public bool Contains(T element)
+    {
+        Node current = FindEelement(element);
+
+        return current != null;
+    }
+
+    public BinarySearchTree<T> Search(T element)
+    {
+        Node current = FindEelement(element);
+
+        if (current == null)
+        {
+            return null;
+        }
+
+        return new BinarySearchTree<T>(current);
+    }
+
+    public void DeleteMin()
+    {
+        if (this.root == null)
+        {
+            return;
+        }
+
+        Node current = this.root;
+        Node parrent = null;
+
+        while (current.Left != null)
+        {
+            parrent = current;
+            current = current.Left;
+        }
+
+        if (parrent == null)
+        {
+            this.root = this.root.Right;
+        }
+        else
+        {
+            parrent.Left = current.Right;
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this.root, action);
+    }
+
+    private void EachInOrder(Node node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Value);
+        this.EachInOrder(node.Right, action);
+    }
+
+    private Node FindEelement(T element)
+    {
+        Node current = this.root;
+
+        while (current != null)
+        {
+            if (current.Value.CompareTo(element) > 0)
+            {
+                current = current.Left;
+            }
+            else if (current.Value.CompareTo(element) < 0)
+            {
+                current = current.Right;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return current;
+    }
+}
+
