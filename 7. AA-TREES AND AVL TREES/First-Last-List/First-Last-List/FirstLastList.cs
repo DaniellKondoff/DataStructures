85ef319129e0eaad97a53d4427eca81250591ad6
DKondoff <danielkondov91gmail.com>
2018-04-16 11:57:34 +0300

    First Last List

--- 7. AA-TREES AND AVL TREES/First-Last-List/First-Last-List/FirstLastList.cs
+++ 7. AA-TREES AND AVL TREES/First-Last-List/First-Last-List/FirstLastList.cs
@@ -1,49 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Wintellect.PowerCollections;
 
 public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
 {
+    private LinkedList<T> byInsertion = new LinkedList<T>();
+    private OrderedBag<LinkedListNode<T>> byOrderSorted = new OrderedBag<LinkedListNode<T>>((x,y) => x.Value.CompareTo(y.Value));
+    private OrderedBag<LinkedListNode<T>> byOrderReversed = new OrderedBag<LinkedListNode<T>>((x,y) => - x.Value.CompareTo(y.Value));
+
     public int Count
     {
         get
         {
-            throw new NotImplementedException();
+            return this.byInsertion.Count;
         }
     }
 
     public void Add(T element)
     {
-        throw new NotImplementedException();
+        var node = new LinkedListNode<T>(element);
+
+        byInsertion.AddLast(node);
+        byOrderSorted.Add(node);
+        byOrderReversed.Add(node);
     }
 
     public void Clear()
     {
-        throw new NotImplementedException();
+        byInsertion.Clear();
+        byOrderSorted.Clear();
+        byOrderReversed.Clear();
     }
 
     public IEnumerable<T> First(int count)
     {
-        throw new NotImplementedException();
+        this.ValidateCount(count);
+
+        return this.byInsertion.Take(count).ToList();
     }
 
     public IEnumerable<T> Last(int count)
     {
-        throw new NotImplementedException();
+        this.ValidateCount(count);
+
+        return this.byInsertion.Reverse().Take(count).ToList();
     }
 
     public IEnumerable<T> Max(int count)
     {
-        throw new NotImplementedException();
+        this.ValidateCount(count);
+
+        return this.byOrderReversed.Take(count).Select(x=>x.Value).ToList();
     }
 
     public IEnumerable<T> Min(int count)
     {
-        throw new NotImplementedException();
+        this.ValidateCount(count);
+
+        return this.byOrderSorted.Take(count).Select(x=>x.Value).ToList();
     }
 
     public int RemoveAll(T element)
     {
-        throw new NotImplementedException();
+        var node = new LinkedListNode<T>(element);
+        var forDeleteRange =  byOrderSorted.Range(node,true,node,true);
+
+
+        foreach (var item in forDeleteRange)
+        {
+            byInsertion.Remove(item);
+        }
+
+        int count = byOrderSorted.RemoveAllCopies(node);
+        byOrderReversed.RemoveAllCopies(node);
+
+        return count;
+    }
+
+    private void ValidateCount(int count)
+    {
+        if (count > this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
     }
 }
